b99024d979bd6ab84f9ee6c37e66c5b9a04e9e68
Jayaram Krishnaswamy <jayaram.linux@gmail.com>
2021-11-10 18:06:22 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Demo Flask & Docker application is up and running! This is the initial version 5 with awesome changes"
+    return "Demo Flask & Docker application is up and running! This is the updated version 1.8 with awesome changes"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
